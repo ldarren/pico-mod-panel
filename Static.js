e0424c134c26a2d90b888954303b804d764385c5
Darren Liew <ldarren@gmail.com>
2016-10-04 10:42:30 +0000

    added static panel

---
+++ Static.js
@@ -0,0 +1,29 @@
+return {
+    tagName:'form',
+    className: 'form',
+    deps:{
+		tpl:'file',
+		models:'refs',
+		fields:'list'
+    },
+    create: function(deps){
+		var
+		models=deps.models,
+		fields=deps.fields
+
+		if (!fields || !models) return
+		
+		var
+		rows=[],
+		i,d,m,v
+
+		for(i=0; d=fields[i]; i++){
+			m=models[d.model]
+			if (!m) continue
+			v=m.get(d.field)
+			v=d.value?v[d.value]:v
+			rows.push({type:'static', label:d.label, value:v})
+		}
+		this.el.innerHTML=deps.tpl(rows)
+    }
+}
