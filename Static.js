b45547cfcb230cb5c66fdf0ed49c44adfe3d7df1
Darren Liew <ldarren@gmail.com>
2016-12-17 16:36:33 +0000

    added button support

--- Static.js
+++ Static.js
@@ -1,3 +1,29 @@
+var
+update=function(){
+	var
+	deps=this.deps,
+	models=deps.models,
+	fields=deps.fields
+
+	if (!fields || !models) return
+	
+	var rows=[]
+
+	for(var i=0,d,m,v; d=fields[i]; i++){
+		if (!d.model){
+			rows.push(d)
+			continue
+		}
+		m=models[d.model]
+		if (!m) continue
+		v=m.get(d.field)
+		if (!v) continue
+		v=d.value?v.get(d.value):v
+		rows.push({type:'static', label:d.label, value:v})
+	}
+	this.el.innerHTML=deps.tpl(rows)
+}
+
 return {
     tagName:'form',
     className: 'form',
@@ -7,24 +33,10 @@ return {
 		fields:'list'
     },
     create: function(deps){
-		var
-		models=deps.models,
-		fields=deps.fields
-
-		if (!fields || !models) return
-		
-		var
-		rows=[],
-		i,d,m,v
-
-		for(i=0; d=fields[i]; i++){
-			m=models[d.model]
-			if (!m) continue
-			v=m.get(d.field)
-			if (!v) continue
-			v=d.value?v[d.value]:v
-			rows.push({type:'static', label:d.label, value:v})
+		for(var i=0,ms=deps.models,m; m=ms[i]; i++){
+			this.listenTo(m,'change',update)
 		}
-		this.el.innerHTML=deps.tpl(rows)
+
+		update.call(this)
     }
 }
